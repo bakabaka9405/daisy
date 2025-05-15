88ad1079794e02bd453d09c4da54e7818580ed70
bakabaka9405 <2677423642@qq.com>
2025-05-15 16:57:37 +0800

    疑似是正确的梯度裁剪

--- daisy/classfier_trainer.py
+++ daisy/classfier_trainer.py
@@ -81,6 +81,7 @@ def fast_train_smile(
 				loss = criterion(outputs, label)
 
 			scaler.scale(loss).backward()
+			scaler.unscale_(optimizer)
 			torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
 			scaler.step(optimizer)
 			scaler.update()
