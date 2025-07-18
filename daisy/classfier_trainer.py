1a9245165f8da6fa0d387409dccc324683d2de13
bakabaka9405 <2677423642@qq.com>
2025-07-18 14:32:04 +0800

    fast_train_smile 支持保存 checkpoint

--- daisy/classfier_trainer.py
+++ daisy/classfier_trainer.py
@@ -6,6 +6,7 @@ from timm.loss.cross_entropy import LabelSmoothingCrossEntropy
 from timm.data.loader import MultiEpochsDataLoader
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import math
+from pathlib import Path
 
 
 def fast_train_smile(
@@ -31,11 +32,16 @@ def fast_train_smile(
 	pin_memory: bool = True,
 	early_stop: bool = False,
 	early_stop_epoch: int = 5,
+	save_path: Path | None = None,
+	keep_count: int = 0,
 ):
 	if train_transform is None:
 		train_transform = daisy.util.transform.get_rectangle_train_transform()
 	if val_transform is None:
 		val_transform = daisy.util.transform.get_rectangle_val_transform()
+
+	if save_path is not None:
+		save_path.mkdir(parents=True, exist_ok=True)
 
 	if isinstance(dataset, tuple):
 		train_dataset, val_dataset = dataset
@@ -72,7 +78,7 @@ def fast_train_smile(
 	criterion = LabelSmoothingCrossEntropy(smoothing=smoothing)
 
 	def lr_func(epoch: int):
-		return min((epoch + 1) / (warmup_epochs + 1e-8), 0.5 * (math.cos(epoch / epochs * math.pi) + 1))
+		return min(epoch / (warmup_epochs + 1e-8), 0.5 * (math.cos(epoch / epochs * math.pi) + 1))
 
 	lr_scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lr_func)
 
@@ -152,6 +158,18 @@ def fast_train_smile(
 		print(
 			f'Epoch {epoch + 1}/{epochs}, LR: {optimizer.param_groups[0]["lr"]:.6f}, Train Loss: {train_losses:.4f}, Train Acc: {train_acc:.4f}, Val Loss: {val_losses:.4f}, Val Accuracy: {val_acc:.4f}, Precision: {prec:.4f}, Recall: {recall:.4f}, F1: {f1:.4f}'
 		)
+
+		if save_path is not None:
+			torch.save(model.state_dict(), save_path / f'model_epoch_{epoch + 1}.pth')
+			# print(f'Model saved at {save_path / f"model_epoch_{epoch + 1}.pth"}')
+			if keep_count > 0 and epoch >= keep_count:
+				remove_path = save_path / f'model_epoch_{epoch + 1 - keep_count}.pth'
+				if remove_path.exists():
+					remove_path.unlink()
+					# print(f'Removed old model {remove_path}')
+
+			if val_acc > best_acc:
+				torch.save(model.state_dict(), save_path / 'best_model.pth')
 
 		if val_acc > best_acc:
 			best_acc = val_acc
